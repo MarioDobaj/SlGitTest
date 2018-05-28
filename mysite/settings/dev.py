ce918bf6c2cff8ff5dfb432b96ae69b24cd89951
MarioDobaj <mario.dobaj@gmail.com>
2018-05-28 18:33:59 +0200

    moved and renamed secret key

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -5,7 +5,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/etc/secret_key.txt') as f:
+with open('key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/etc/secret_key.txt') as f:
+with open('key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 try:
